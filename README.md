b01ad439922bfbdfa223e1430c8d3fa477d85f52
dngtnv <21217751+dngtnv@users.noreply.github.com>
2024-06-08 18:05:52 +0700

    Server application

---
+++ README.md
@@ -0,0 +1,3 @@
+# Server Application
+
+This server application is built using Node.js and Express, providing a RESTful API for a social media platform. It supports user authentication, posting feeds, and uploading images.
---
+++ app.js
@@ -0,0 +1,68 @@
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
+const multer = require('multer');
+const helmet = require('helmet');
+
+const feedRoutes = require('./routes/feed');
+const authRoutes = require('./routes/auth');
+
+const MONGODB_URI = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PW}@cluster0.m8gjdlk.mongodb.net/${process.env.MONGO_DEFAULT_DB}?retryWrites=true&w=majority&appName=Cluster0`;
+
+const app = express();
+
+const fileStorage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'images');
+  },
+  filename: (req, file, cb) => {
+    cb(null, new Date().toISOString().replace(/:/g, '-') + '-' + file.originalname);
+  },
+});
+
+const fileFilter = (req, file, cb) => {
+  if (file.mimetype === 'image/png' || file.mimetype === 'image/jpg' || file.mimetype === 'image/jpeg') {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
+
+app.use(helmet());
+app.use(cors());
+app.use(bodyParser.json());
+app.use(multer({ storage: fileStorage, fileFilter: fileFilter }).single('image'));
+app.use('/images', express.static(path.join(__dirname, 'images')));
+
+app.use('/feed', feedRoutes);
+app.use('/auth', authRoutes);
+
+// Checkhealth
+app.get('/ping', (req, res) => {
+  res.status(200).json({ response: 'pong' });
+});
+
+app.use((error, req, res, next) => {
+  console.log(error);
+  const status = error.statusCode || 500;
+  const message = error.message;
+  const data = error.data;
+  res.status(status).json({ message: message, data: data });
+});
+
+mongoose
+  .connect(MONGODB_URI)
+  .then((result) => {
+    const server = app.listen(process.env.PORT || 5000, () => {
+      console.log(`Server is running on port ${process.env.PORT || 5000}`);
+    });
+    const io = require('./socket.js').init(server);
+    io.on('connection', (socket) => {
+      console.log('Client connected');
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
